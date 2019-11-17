59bec328e8ea746d33e33bf5e6b1debb3d19fa07
Stacey Torres <torress@tcd.ie>
2019-11-17 14:09:46 +0000

    Update Nov 17

--- bootstrap-website/new-js/index.js
+++ bootstrap-website/new-js/index.js
@@ -1,4 +1,4 @@
-//All about the back to top button:
+// this is about the back to top button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -14,13 +14,12 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
-//Get the button:
+
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -36,10 +35,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-// $(document).ready(function () {
-//   $("li.a.scrollLink").click(function (event) {
-//       event.preventDefault();
-//       $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
-//   });
-// });
